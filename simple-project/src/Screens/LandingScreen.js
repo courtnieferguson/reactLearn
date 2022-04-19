be6e66df5a829d085f751a6fb16ae255a98f0428
courtnieferguson <84743680+courtnieferguson@users.noreply.github.com>
2022-04-19 15:15:06 -0500

    simple-project

---
+++ simple-project/src/Screens/LandingScreen.js
@@ -0,0 +1,13 @@
+const LandingScreen = ()=>{
+    return(
+    <div style={{
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "blue"
+    }}>
+
+    </div>
+    )
+}
+
+export default LandingScreen
